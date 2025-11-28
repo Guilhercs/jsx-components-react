8c44331326596d9ab5655bd988391d26154a8c3c
guilhercs <guilherme.carvalhos@outlook.com>
2025-11-28 18:19:32 -0300

    feat: create app

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <main>
+        <header>
+          <img src="/header-desktop.png" alt="header" />
+        </header>
+        <section>
+          <img src="/Hero Image com texto_L.png" alt="" />
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
