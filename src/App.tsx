415e1cc873fad086d44bf575d8621bb5f840a1f0
guilhercs <guilherme.carvalhos@outlook.com>
2025-11-29 20:30:05 -0300

    feat(form): add event form

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,40 @@
 import "./App.css";
 
+function FormularioDeEvento() {
+  return (
+    <>
+      <form className="form">
+        <h2>Preencha para criar um evento:</h2>
+        <fieldset>
+          <label htmlFor="nome">Qual o nome do evento?</label>
+          <input type="text" id="nome" />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="nome">Data do evento</label>
+          <input type="text" id="data" />
+        </fieldset>
+        <fieldset>
+          <label htmlFor="nome">Tema do evento</label>
+          <input type="text" id="tema" />
+        </fieldset>
+      </form>
+    </>
+  );
+}
+
 function App() {
   return (
     <>
       <main>
-        <header>
+        <header className="header">
           <img src="/header-desktop.png" alt="header" />
         </header>
         <section>
           <img src="/Hero Image com texto_L.png" alt="" />
         </section>
+        <div>
+          <FormularioDeEvento />
+        </div>
       </main>
     </>
   );
