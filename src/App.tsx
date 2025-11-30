121efbc1633b300e5b08c478429783c600a21207
guilhercs <guilherme.carvalhos@outlook.com>
2025-11-30 16:11:52 -0300

    feat(components): add types components

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,55 @@
+import type React from "react";
 import "./App.css";
+
+function TitleCard(props: React.HTMLAttributes<HTMLHeadingElement>) {
+  return (
+    <>
+      <h2 {...props}>{props.children}</h2>
+    </>
+  );
+}
+
+function FieldSet(props: React.FieldsetHTMLAttributes<HTMLFieldSetElement>) {
+  return (
+    <>
+      <fieldset>{props.children}</fieldset>
+    </>
+  );
+}
+
+function Label(props: React.LabelHTMLAttributes<HTMLLabelElement>) {
+  return (
+    <>
+      <label htmlFor={props.htmlFor}>{props.children}</label>
+    </>
+  );
+}
+
+function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <>
+      <input {...props} />
+    </>
+  );
+}
 
 function FormularioDeEvento() {
   return (
     <>
       <form className="form">
-        <h2>Preencha para criar um evento:</h2>
-        <fieldset>
-          <label htmlFor="nome">Qual o nome do evento?</label>
-          <input type="text" id="nome" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="nome">Data do evento</label>
-          <input type="text" id="data" />
-        </fieldset>
-        <fieldset>
-          <label htmlFor="nome">Tema do evento</label>
-          <input type="text" id="tema" />
-        </fieldset>
+        <TitleCard>Preencha para criar um evento:</TitleCard>
+        <FieldSet>
+          <Label htmlFor="nome">Qual o nome do evento?</Label>
+          <Input type="text" id="nome" />
+        </FieldSet>
+        <FieldSet>
+          <Label htmlFor="nome">Data do evento</Label>
+          <Input type="text" id="data" />
+        </FieldSet>
+        <FieldSet>
+          <Label htmlFor="nome">Tema do evento</Label>
+          <Input type="text" id="tema" placeholder="Summer dev hits" />
+        </FieldSet>
       </form>
     </>
   );
